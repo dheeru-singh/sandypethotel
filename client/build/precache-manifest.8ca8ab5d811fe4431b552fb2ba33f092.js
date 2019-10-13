063976568dbd2902455093c0a32f90ff2f75153f
Dheeru Singh <dheeru.singh228@gmail.com>
2019-10-13 01:13:57 -0400

    initial commit

---
+++ client/build/precache-manifest.8ca8ab5d811fe4431b552fb2ba33f092.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7f10ff78e20f74b20dfeb6e3b6c9d9d",
+    "url": "/static/media/dogs.b7f10ff7.jpg"
+  },
+  {
+    "revision": "3a4621d675987741af92",
+    "url": "/static/css/main.ff915bf8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21ca88e34e863316c35e",
+    "url": "/static/js/2.8353868d.chunk.js"
+  },
+  {
+    "revision": "f2686b2afdd63e515502de1b879abe0c",
+    "url": "/static/media/la.f2686b2a.jpg"
+  },
+  {
+    "revision": "8c6f42f487dbc681207520d9c7ab3efb",
+    "url": "/static/media/SandyspetHotel_LOGO.8c6f42f4.png"
+  },
+  {
+    "revision": "3a4621d675987741af92",
+    "url": "/static/js/main.bbfe2f3d.chunk.js"
+  },
+  {
+    "revision": "f221bb8072177daa4d1db12d00c149c8",
+    "url": "/static/media/cats.f221bb80.jpg"
+  },
+  {
+    "revision": "7a7e6b5f00eb6843c05c5c6446e1e6dd",
+    "url": "/static/media/smallanimal.7a7e6b5f.jpg"
+  },
+  {
+    "revision": "0ebe162621264b60031146a42a0e93b3",
+    "url": "/static/media/birds.0ebe1626.jpg"
+  },
+  {
+    "revision": "25ba66b5bb184aa6a63686bd0a6fea0a",
+    "url": "/static/media/reptiles.25ba66b5.jpg"
+  },
+  {
+    "revision": "97baf7d0985a9136120915dec1b4c005",
+    "url": "/static/media/fish.97baf7d0.jpg"
+  },
+  {
+    "revision": "dd7caead80ecafd560ec316d05ea94b7",
+    "url": "/index.html"
+  }
+];
